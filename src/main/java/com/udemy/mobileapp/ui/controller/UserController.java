55dc884737d4144011b72c7760828612542d5df7
mohamedfazilk <2114921@cognizant.com>
2022-09-27 22:24:26 +0530

    place @RequestParam in the controller

--- src/main/java/com/udemy/mobileapp/ui/controller/UserController.java
+++ src/main/java/com/udemy/mobileapp/ui/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	@GetMapping
-	public String getUser() {
-		return "hello users";
+	public String getUser(@RequestParam(value = "page")int page,
+			@RequestParam(value = "limit")int limit) {
+		return "hello users "  + page + "and" + limit;
 	}
 	
 	@PostMapping
